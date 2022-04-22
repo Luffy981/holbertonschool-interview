62101d1fdf322b7b009305b2f3fae60ae18b4eb9
luffy <jhonsmith981@hotmail.com>
2022-04-21 23:59:19 -0500

    all together

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,8 +7,6 @@ def canUnlockAll(boxes):
     unlocked = boxes[0]
     for box_id, keys in enumerate(boxes):
         if not keys:
-            if box_id in unlocked:
-                unlocked.append(0)
             continue
         for key in keys:
             if key < len(boxes) and key not in unlocked and key != box_id:
