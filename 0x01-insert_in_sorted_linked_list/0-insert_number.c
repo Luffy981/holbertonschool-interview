94a19e4e5f198c35d9a22b5ebd035b5fdd4abb60
luffy <jhonsmith981@hotmail.com>
2022-04-28 17:04:14 -0500

    insert in sorted linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -5,35 +5,46 @@
  * @number: number of node inserted
  * Return: listint_t*
  */
-listint_t *insert_node(listint_t **head, int number)
+listint_t *insert_node_recursive(listint_t **head, int number)
 {
-listint_t *node = NULL;
-listint_t *newNode = NULL;
-
-if (head == NULL)
-{
-return (NULL);
-}
-node = *head;
-newNode = malloc(sizeof(listint_t));
-if (newNode == NULL)
-{
-return (NULL);
-}
-newNode->n = number;
-if (!node || node->n > number)
-{
-newNode->next = *head;
-*head = newNode;
-} else
-{
-while (node->next->n < number && node->next != NULL)
-{
-node = node->next;
-}
-newNode->n = number;
-newNode->next = node->next;
-node->next = newNode;
-}
-return (newNode);
+	listint_t *node = NULL;
+	/*
+    * insert_node:: (NULL, number) -> NULL
+    */
+	if (!head)
+	{
+		return (NULL);
+	}
+	/*
+    * insert_node:: (*NULL, number) -> NULL
+    */
+	if (!*head)
+	{
+		node = malloc(sizeof(listint_t)), node->next = NULL, node->n = number;
+		*head = node;
+		return (node);
+	}
+	/*
+    * insert_node:: (*{ n } : *NULL, number) | n > number -> *listint_t
+    */
+	if ((*head)->n > number)
+	{
+		node = malloc(sizeof(listint_t)), node->next = NULL, node->n = number;
+		node->next = (*head), *head = node;
+		return (node);
+	}
+	/*
+    * insert_node:: (_: *{ n } : list, number) | n > number -> *listint_t
+    */
+	if ((*head)->next && (*head)->next->n > number)
+	{
+		node = malloc(sizeof(listint_t)), node->next = NULL, node->n = number;
+		node->next = (*head)->next, (*head)->next = node;
+		return (node);
+	}
+	/*
+    * insert_node:: (_ : [*listint_t], number)
+	*               -> insert_node(listint_t, number)
+    */
+	return (insert_node(&((*head)->next), number));
 }
