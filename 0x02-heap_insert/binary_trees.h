4007d7db0972562ec2513d436100c360570650d1
luffy <jhonsmith981@hotmail.com>
2022-05-05 20:51:13 -0500

    header tree

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -24,9 +24,9 @@ struct binary_tree_s
 typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s binary_tree_t;
 
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+void binary_tree_print(const binary_tree_t *tree);
 
 /**
  * SWAP_TREE_NODE - macro that can be used to swap a node if binary tree
